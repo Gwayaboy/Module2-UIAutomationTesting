eb7682d599fedba3469f7903fb5ad959589d0a07
Franck Théolade <ftheolade@gmail.com>
2020-06-17 23:23:25 +0100

    added specflow skeleton

---
+++ Sources/Labs/BingWebSearchWithSpecFlow/BingWebSearchWithSpecFlow/Features/WebBingSearchEngine.feature
@@ -0,0 +1,27 @@
+﻿Feature: Web Bing Search engine text search
+    As a User
+    I want to be able to perform searches 
+    So that I can find the information I need easily
+
+Background: 
+  Given the user navigated to the url "http://bing.com/" 
+
+Scenario: Empty Search Text
+
+When the user submits the search 
+Then the the URL should remain "http://bing.com/"
+  And the URL should not contains "search?q="
+  And the Page's title should be "Bing"
+
+Scenario: Current Pin location search
+
+  When the user submits the search 
+  Then the results related to the background's image location should be listed
+
+
+Scenario: "Hello World" Search Text
+
+  Given the user typed "Hello World!" 
+  When the user submits the search 
+  Then more than 1 result(s) should be listed 
+    And one of the result item's title should contain "Hello, World! program - Wikipedia"
---
+++ Sources/Labs/BingWebSearchWithSpecFlow/BingWebSearchWithSpecFlow/Pages/Page.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingWebSearchWithSpecFlow.Pages
+{
+
+    public abstract class Page
+    {
+        protected IWebDriver WebDriver { get; private set; }
+
+        public const int DefaultSecondTimeout = 60;
+
+        public string Title => WaitUntil(d => d.Title);
+
+        public string Url => WebDriver.Url;
+
+        protected TPage GoTo<TPage>(By byLocator, Action<IWebElement> performAction = null)
+           where TPage : Page, new()
+        {
+            var action = performAction ?? (e => e.Click());
+            action(FindElement(byLocator));
+
+            return new TPage { WebDriver = WebDriver };
+        }
+
+        protected IWebElement FindElement(By byLocator, TimeSpan maxWait = default)
+        {
+            try
+            {
+                return WaitUntil(d => d.FindElement(byLocator));
+            }
+            catch (WebDriverTimeoutException e)
+            {
+                throw e.InnerException;
+            }
+        }
+
+        protected void ExecuteScript(string scriptToExecute)
+        {
+            var javascriptExecutor = (IJavaScriptExecutor)WebDriver;
+
+            var untypedValue = javascriptExecutor.ExecuteScript(scriptToExecute);
+
+        }
+
+        internal static TPage GoToInitial<TPage>(string startUpUrl, IWebDriver webDriver)
+                where TPage : Page, new()
+        {
+            if (webDriver == null) throw new ApplicationException("Please provide with an instance of web driver to proceed");
+            if (string.IsNullOrWhiteSpace(startUpUrl)) throw new ApplicationException("Please provide with a start up url");
+
+            webDriver.Navigate().GoToUrl(startUpUrl);
+
+            return new TPage { WebDriver = webDriver };
+        }
+
+        private TReturn WaitUntil<TReturn>(Func<IWebDriver, TReturn> elementFinder, TimeSpan maxWait = default)
+        {
+            if (maxWait == default)
+            {
+                maxWait = TimeSpan.FromSeconds(DefaultSecondTimeout);
+            }
+            var wait = new WebDriverWait(WebDriver, maxWait);
+            return wait.Until(elementFinder);
+        }
+    }
+}
